45ac82873a3b2451c5e56413c3acbf5673e016c4
Bsetecip10 <bsetecip10@Nirmals-Mac-mini.local>
2014-11-06 12:10:47 +0530

    Initial Commit

---
+++ ApplicationSettingTests/ApplicationSettingTests.m
@@ -0,0 +1,40 @@
+//
+//  ApplicationSettingTests.m
+//  ApplicationSettingTests
+//
+//  Created by Bsetecip10 on 06/11/14.
+//  Copyright (c) 2014 Bsetecip10. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ApplicationSettingTests : XCTestCase
+
+@end
+
+@implementation ApplicationSettingTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
